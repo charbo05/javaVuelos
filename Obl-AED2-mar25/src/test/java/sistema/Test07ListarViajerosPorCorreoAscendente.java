810cd82eb79edb54aa600eed983300506d9cf9ae
Maxi <maxivitale@icloud.com>
2025-04-20 11:52:19 -0300

    Listar por correo

--- Obl-AED2-mar25/src/test/java/sistema/Test07ListarViajerosPorCorreoAscendente.java
+++ Obl-AED2-mar25/src/test/java/sistema/Test07ListarViajerosPorCorreoAscendente.java
@@ -24,7 +24,7 @@ public class Test07ListarViajerosPorCorreoAscendente {
         void listarViajeroCorreosOk() {
         s.registrarViajero("3.914.689-5", "Guillermo", "guille@ort.edu.uy", 35, Categoria.ESTANDAR);
         s.registrarViajero("2.914.689-5", "Hugo", "hugo@ort.edu.uy", 35, Categoria.ESTANDAR);
-        s.registrarViajero("1.914.689-5", "Guillermo", "asma@ort.edu.uy", 35, Categoria.ESTANDAR);
+        s.registrarViajero("5.914.689-5", "Guillermo", "asma@ort.edu.uy", 35, Categoria.ESTANDAR);
 
         retorno = s.listarViajerosPorCorreoAscendente();
 
@@ -32,7 +32,7 @@ public class Test07ListarViajerosPorCorreoAscendente {
 
         // assertEquals(1, retorno.getValorInteger());
 
-        assertEquals("1.914.689-5;Guillermo;asma@ort.edu.uy;35;Estándar|3.914.689-5;Guillermo;guille@ort.edu.uy;35;Estándar|2.914.689-5;Hugo;hugo@ort.edu.uy;35;Estándar", retorno.getValorString());
+        assertEquals("5.914.689-5;Guillermo;asma@ort.edu.uy;35;Estándar|3.914.689-5;Guillermo;guille@ort.edu.uy;35;Estándar|2.914.689-5;Hugo;hugo@ort.edu.uy;35;Estándar", retorno.getValorString());
 
             System.out.println(retorno);
 
@@ -43,19 +43,53 @@ public class Test07ListarViajerosPorCorreoAscendente {
         @Test
         void listarViajeroCorreoDesordenadosOk() {
         s.registrarViajero("2.914.689-5", "Guillermo", "biron@ort.edu.uy", 35, Categoria.ESTANDAR);
-        s.registrarViajero("3.914.689-5", "Guillermo", "guille2@ort.edu.uy", 35, Categoria.ESTANDAR);
-        s.registrarViajero("1.914.689-5", "Guillermo", "antunez3@ort.edu.uy", 35, Categoria.ESTANDAR);
+        s.registrarViajero("1.914.689-5", "Guillermo", "guille2@ort.edu.uy", 35, Categoria.ESTANDAR);
+        s.registrarViajero("3.914.689-5", "Guillermo", "antunez3@ort.edu.uy", 35, Categoria.ESTANDAR);
 
-        retorno = s.listarViajerosPorCedulaAscendente();
+        retorno = s.listarViajerosPorCorreoAscendente();
 
         assertEquals(Retorno.Resultado.OK, retorno.getResultado());
 
 
 
-        assertEquals("1.914.689-5;Guillermo;antunez3@ort.edu.uy;35;Estándar|2.914.689-5;Guillermo;biron@ort.edu.uy;35;Estándar|3.914.689-5;Guillermo;guille2@ort.edu.uy;35;Estándar", retorno.getValorString());
+        assertEquals("3.914.689-5;Guillermo;antunez3@ort.edu.uy;35;Estándar|2.914.689-5;Guillermo;biron@ort.edu.uy;35;Estándar|1.914.689-5;Guillermo;guille2@ort.edu.uy;35;Estándar", retorno.getValorString());
 
 
+            System.out.println(retorno);
 
     }
+
+    @Test
+    void listarViajerosPorCorreoAscendente_variosViajeros_ordenCorrecto() {
+        s.inicializarSistema(5);
+
+        s.registrarViajero("4.000.000-0", "Ana", "zzz@correo.com", 22, Categoria.ESTANDAR);
+        s.registrarViajero("3.000.000-0", "Luis", "aaa@correo.com", 30, Categoria.PLATINO);
+        s.registrarViajero("5.000.000-0", "Sofía", "mmm@correo.com", 27, Categoria.FRECUENTE);
+
+        Retorno retorno = s.listarViajerosPorCorreoAscendente();
+
+        assertEquals(Retorno.Resultado.OK, retorno.getResultado());
+
+        assertEquals(
+                "3.000.000-0;Luis;aaa@correo.com;30;Platino|" +
+                        "5.000.000-0;Sofía;mmm@correo.com;27;Frecuente|" +
+                        "4.000.000-0;Ana;zzz@correo.com;22;Estándar",
+                retorno.getValorString()
+        );
+        System.out.println(retorno);
     }
+
+    @Test
+    void listarViajerosPorCorreoAscendente_sinViajeros() {
+        s.inicializarSistema(5);
+
+        Retorno retorno = s.listarViajerosPorCorreoAscendente();
+
+        assertEquals(Retorno.Resultado.OK, retorno.getResultado());
+        assertEquals("", retorno.getValorString());
+    }
+
+
+}
 

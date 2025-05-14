0633332eaac2e197540766bc945b42468cb53b98
Christian Charbonnier <maxivitale@icloud.com>
2025-05-14 00:09:09 -0300

    Algunos comentarios en el codigo

--- Obl-AED2-mar25/src/main/java/tadsAux/ResultadoBusqueda.java
+++ Obl-AED2-mar25/src/main/java/tadsAux/ResultadoBusqueda.java
@@ -1,8 +1,10 @@
 package tadsAux;
 
+/*Clase auxiliar genérica para encapsular dos elementos fundamentales que se obtienen al realizar una búsqueda*/
 public class ResultadoBusqueda<T> {
-    private T dato;
-    private int comparaciones;
+
+    private T dato; // El resultado (puede ser un vuelo, una ciudad, etc.)
+    private int comparaciones; // Número de comparaciones realizadas en la búsqueda
 
     public ResultadoBusqueda(T dato, int comparaciones) {
         this.dato = dato;
